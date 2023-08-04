97753f5284bb90d713652a56e36afb230390e033
samuel <samugit.projects@gmail.com>
2023-08-04 20:02:11 -0300

    Aula02 Criando primeiro array em Java

---
+++ src/arrays/ArrayApp1.java
@@ -0,0 +1,14 @@
+package arrays;
+
+public class ArrayApp1 {
+
+    public static void main(String[] args) {
+
+        int[] array = new int[5];
+        array[0] = 2;
+        array[4] = 10;
+
+        System.out.println(array[0]);
+        System.out.println(array[4]);
+    }
+}
