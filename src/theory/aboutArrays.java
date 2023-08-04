e11d0f4b8d3584cff550127d6d2929d3741105ce
samuel <samugit.projects@gmail.com>
2023-08-03 23:17:32 -0300

    Aula01 Conhecendo as características de um array

---
+++ src/theory/aboutArrays.java
@@ -0,0 +1,19 @@
+package theory;
+
+@SuppressWarnings("ALL")
+public class aboutArrays {
+
+    /*
+    * Um Array é uma coleção de dados de um tipo específico (ou seja todos os elementos são sempre do mesmo tipo)
+    * os dados ficam armazenados na memória de forma contígua (um ao lado do outro na memória)
+    * são representados visualmente pelo professor por um retangulo dividido internamente em várias "caixas"
+    * cada espaço é indexado começando em 0, de forma que o último elemento é sempre o tamanho do array -1
+    * por exemplo um array  de 6 Strings é indexado de 0 a 5
+    * o tamanho do array deve ser definido na criação do mesmo porque o Java não faz redimentsionamento dessa estrutura
+    * ou seja possuem um tamanho fixo (isso ocorre devido reservar uma área contígua da memória para os elementos que se que trabalhar)
+    * pode-se ter arrays de tipos primitivos, porém, independente dos elementos contidos nesse array sempre é um objeto no heap
+    * de forma que as variáveis definidas para esse array na stack apontam para o endereço de memória desse objeto no heap
+    * essa área de memória aponta para o primeiro elemento do array, exemplo String [] s...
+    *
+    * */
+}
