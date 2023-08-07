ccbb831e7f1fee4847353033aefeeed20aa0f795
samuel <samugit.projects@gmail.com>
2023-08-07 12:27:38 -0300

    Aula08 Como funcionam os arrays multidimensionais

---
+++ src/theory/multidimensionalArrays.java
@@ -0,0 +1,29 @@
+package theory;
+@SuppressWarnings("ALL")
+public class multidimensionalArrays {
+
+    /*
+    * array de 1 dimensão ( exemplo int[] = new int [5]; )
+    *  0 |_|
+     * 1 |_|
+     * 2 |_|
+     * 3 |_|
+     * 4 |_|
+    *
+    *
+    * array de duas dimensões ( exemplo int[][] x = new int[5][3]; )
+    *    0 1 2
+    * 0 |_|_|_|
+    * 1 |_|_|_|
+    * 2 |_|_|_|
+    * 3 |_|_|_|
+    * 4 |_|_|_|
+    *
+    * imaginar o conceito de batalha naval, ou do funcionamento de uma planilha...
+    *
+    *
+    * arrays de 3 dimensões ou mais é muito raro mas o funcionamento segue o mesmo conceito
+    * no caso de 3 dimensões é só imaginar um cubo em 3D
+    *
+    */
+}
