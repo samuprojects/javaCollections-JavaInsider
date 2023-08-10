a2eb5e2008378935b6120432fa9360a35ce3ac72
samuel <samugit.projects@gmail.com>
2023-08-10 14:13:40 -0300

    Aula26 Comparando ArrayLists e LinkedLists Parte 2

---
+++ src/theory/ComparingArraysListsLinkedLists2.java
@@ -0,0 +1,17 @@
+package theory;
+
+@SuppressWarnings("ALL")
+public class ComparingArraysListsLinkedLists2 {
+
+    /*
+    * comparativo
+    *                   ArrayList                 LinkedList
+    * search            0 1 2 3                 0  ->  1  -> 2
+    *                  |_|_|_|_|
+    *
+    * na busca pelo índice o ArrayList é melhor pois é indexado, já o LinkedList não possui índice obrigando a percorrer toda lista
+    * para encontrar o elemento desejado
+    *
+    * Já na busca pelo valor armazenado dentro do Array ou List dá empate pois em ambas estruturas tem que percorrer 1 a 1
+    * */
+}
