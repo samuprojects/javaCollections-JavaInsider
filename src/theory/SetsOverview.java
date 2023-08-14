e2326ac549499ec7f0277a525635c8382223d45d
samuel <samugit.projects@gmail.com>
2023-08-14 15:30:43 -0300

    Aula29 Uma visão geral sobre os conjuntos

---
+++ src/theory/SetsOverview.java
@@ -0,0 +1,29 @@
+package theory;
+
+@SuppressWarnings("ALL")
+public class SetsOverview {
+
+    /* O Set é a representação, na programação, do que conhecemos na matemática como Conjuntos
+
+    Um conjunto é um agrupamento de informações
+
+    - Não permite duplicações;
+    - Não há ordem
+
+    É como se fosse um "saco" onde as coisas ficam armazenadas lá dentro
+
+    O Set, assim como o List, é apenas uma Interface, que tem algumas implementações com diferentes propósitos
+
+    A mais famosa é a HashSet, depois a LinkedHasSet e o TreeSet. Pode-se observar na estrutura dos nomes que:
+    os sufixos Set remetem ao tipo de coleção (nesse caso o Set, assim como tivemos o List... "ArrayList",etc)
+    e os prefixos remetem ao algoritmo, ou seja, à forma como as coisas funcionam dentro do Set
+
+    Por exemplo o HashSet utiliza um algoritmo de Hashing (que é um algoritmo de espalhamento) para armazenar as informações dentro do cojunto
+
+    O LinkedHashSet também utiliza um algoritmo de hash, porém, com "linkagem" que serve para quando você tem necessidade de retornar os valores
+    com uma certa ordenação
+
+    O TreeSet possui um algoritmo de ordenação chamado de RedBlackTree que ordena os elementos conforme são adicionados ao conjunto automaticamente
+
+     */
+}
