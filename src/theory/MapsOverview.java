f3d5580867043c2f275e71995800dcf577043fae
samuel <samugit.projects@gmail.com>
2023-08-23 18:19:05 -0300

    Aula40 Uma visão geral sobre os mapas

---
+++ src/theory/MapsOverview.java
@@ -0,0 +1,29 @@
+package theory;
+
+@SuppressWarnings("ALL")
+public class MapsOverview {
+
+    /*
+    *
+    * Maps
+    *
+    * Muito utilizado para mapear um elemento a outro elemento
+    *
+    * Exemplo: Um grupo de pessoas identifica cada uma delas , o CPF
+    *
+    * CPF mapeado para uma -> Pessoa
+    * 123 -> P1
+    * 456 -> P2
+    * 678 -> P3
+    * KEYS -> VALUES
+    *
+    * Em maps temos do lado esquerdo as chaves associado aos values do lado direito, sempre uma key associado a um value
+    * por isso as keys são únicas, não podem ser duplicadas
+    * como não são duplicados as chaves são um conjunto de Set
+    * já os values podem ser repetidos e como não são sequenciais são tratados como uma Collection (a superclasse do Java para coleções)
+    *
+    * Keys --- Values    \_ esses conjuntos formam um par, uma entrada de um map (Entry), quais são os pares de chave/valor que aquele mapa tem
+    * Set     Collection /
+    *
+    * */
+}
